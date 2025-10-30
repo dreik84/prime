f470302f98b028cc6ee2628414ef384ca4c79370
dreik84 <dreik84@mail.ru>
2025-10-30 16:29:40 +0300

    Update PrimeApp.java

--- src/main/java/org/example/PrimeApp.java
+++ src/main/java/org/example/PrimeApp.java
@@ -4,10 +4,16 @@ import java.util.List;
 
 public class PrimeApp {
     public static void main(String[] args) {
+        int bound = 10_000_000;
 
-        List<Integer> primeNumbers = Prime.generate(1000);
+        long start = System.currentTimeMillis();
+
+        List<Integer> primeNumbers = Prime.generate(bound);
+
+        long end = System.currentTimeMillis();
 
         System.out.println(primeNumbers);
-        System.out.println(primeNumbers.size());
+        System.out.println("Count: " + primeNumbers.size());
+        System.out.println("Time: " + (end - start) + " ms");
     }
 }
